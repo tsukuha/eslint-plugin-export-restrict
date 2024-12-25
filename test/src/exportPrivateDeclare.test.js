99c8913be989bc20e2583a0ed17c7dbb63a2eed7
Tsukuha Kawakami <tsukuha0727@gmail.com>
2024-12-25 21:39:25 +0900

    add test

---
+++ test/src/exportPrivateDeclare.test.js
@@ -0,0 +1,52 @@
+
+/** @private */
+export function a() {}
+
+/** @private */
+export class c {}
+
+function ok4() {}
+
+/** @private */
+export const abcd = {};
+
+const abcOK = {};
+
+/** @private */
+const abcde = {};
+
+/** @private */
+export const vv = {};
+
+/** @private */
+export const { d, e } = { d: '', e: '' };
+
+function ok1() {}
+
+/** @private */
+export async function asa() {}
+
+/** @private */
+class v {}
+
+/** @private */
+function w() {}
+
+export async function ok2() {}
+
+/**
+ * @private
+ * 
+ */
+function z() {}
+
+function ok3() {}
+
+export {
+  v,
+  z,
+  ok1,
+  w,
+  ok3,
+  abcde,
+};
