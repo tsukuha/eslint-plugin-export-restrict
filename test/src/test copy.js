4cb7c06c27b95a588395c8c7e29101ef94a87867
Tsukuha Kawakami <tsukuha0727@gmail.com>
2024-12-26 09:37:22 +0900

    add tests

---
+++ test/src/test copy.js
@@ -0,0 +1,32 @@
+/** @private */
+export const a = {};
+
+class okClass {}
+
+function okFunction() {}
+
+/** @private */
+export class b {};
+
+/** @private */
+function c() {}
+
+const okVar = {};
+
+/** @private */
+const out = {};
+
+function okD() {}
+
+/** @private */
+class outE {}
+
+export {
+    okClass,
+    outE,
+    okFunction,
+    okVar,
+    out,
+    okD,
+    c,
+};
