6ede8aa7d714834daa8fe17b2b9f92a733f90d8f
Tsukuha Kawakami <tsukuha0727@gmail.com>
2025-05-18 17:41:25 +0900

    feat: add eslint-plugin-vue-test

---
+++ test/eslint-plugin-vue.test.config.js
@@ -0,0 +1,22 @@
+import exportRestrictPlugin from "../dist/esm/index.js";
+import typescriptESLintParser from "@typescript-eslint/parser";
+import vueEslintParser from 'vue-eslint-parser';
+
+export default [
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.vue"],
+    plugins: {
+      "export-restrict": exportRestrictPlugin,
+    },
+    rules: {
+      "export-restrict/no-export-private-declare": ["error"],
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        parser: typescriptESLintParser,
+        extraFileExtensions: ['.vue'],
+      },
+    },
+  },
+];
